4d3ab0f1d418d65f36bfd3689b18932bc39ff680
Harish Kumar <harish.sukant88@gmail.com>
2022-01-14 11:01:36 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {	
+		
+		@Test
+		public void BrowserAutomation()
+		{
+			System.out.println("Web Automation");
+		}
+		
+		@Test
+		public void ElementsUI()
+		{
+			System.out.println("Web UI Automation");
+		}
+		
+}
